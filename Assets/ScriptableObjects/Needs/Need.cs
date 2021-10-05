5b7325d2e2cb13dfc963caba2cdad36bca99ec9b
Dylan Landry <dylanglandry@gmail.com>
2021-10-05 19:06:02 -0400

    feat: create needs

---
+++ Assets/ScriptableObjects/Needs/Need.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Need", menuName = "ScriptableObjects/Need")]
+public class Need : ScriptableObject
+{
+    public string label;
+    public float value;
+}
