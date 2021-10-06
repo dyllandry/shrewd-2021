0ad314b699c1e583a888fceb7b97fd34270c40fd
Dylan Landry <dylanglandry@gmail.com>
2021-10-05 21:20:32 -0400

    feat: needs decrease over time

--- Assets/ScriptableObjects/Needs/Need.cs
+++ Assets/ScriptableObjects/Needs/Need.cs
@@ -5,6 +5,35 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Need", menuName = "ScriptableObjects/Need")]
 public class Need : ScriptableObject
 {
-    public string label;
-    public float value;
+    [SerializeField]
+    string label;
+    public string Label { get => label; set => label = value; }
+
+    [SerializeField]
+    [Range(0, 1)]
+    float value;
+    public float Value
+    {
+        get => this.value;
+        set => this.value = Mathf.Clamp01(value);
+    }
+
+    [Tooltip("How many minutes it takes for the need to drop to 0. Leave at 0 if the need should not decay.")]
+    [SerializeField]
+    float decayRate;
+    public void Decay()
+    {
+        if (this.decayRate == 0) return;
+        this.Value -= 1 / (this.decayRate * 60) * Time.deltaTime;
+    }
+
+    public string GetLabel()
+    {
+        return this.Label;
+    }
+
+    public void Reset()
+    {
+        this.Value = 1;
+    }
 }
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    List<Need> Needs;
+
+    void Start()
+    {
+        foreach (Need need in this.Needs) need.Reset();
+    }
+
+    void Update()
+    {
+        foreach (Need need in this.Needs) need.Decay(); 
+    }
+}
