496ffda2e931d18de20d3705546fdbf75a65207e
Dylan <dylanglandry@gmail.com>
2021-10-13 02:27:56 -0400

    feat: ui blocks click to move

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -2,19 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
 public class ClickToMove : MonoBehaviour
 {
 
     private NavMeshAgent navMeshAgent;
 
-    // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -22,6 +21,9 @@ public class ClickToMove : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
+            // Checks if pointer is over an EventSystem object (part of the UI).
+            if (EventSystem.current.IsPointerOverGameObject()) return;
+
             if (Physics.Raycast(ray, out hit, 100))
             {
                 navMeshAgent.destination = hit.point;
