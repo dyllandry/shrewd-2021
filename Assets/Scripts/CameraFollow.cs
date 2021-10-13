97f94254d4672e8e29c29aa54e95471544811fe3
Dylan <dylanglandry@gmail.com>
2021-10-13 02:46:34 -0400

    feat: camera follows character

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    Transform character;
+
+    Vector3 cameraOffset;
+
+    private void OnEnable()
+    {
+        this.cameraOffset = this.transform.position - this.character.position;
+    }
+
+    private void Update()
+    {
+        this.transform.position = this.character.position + this.cameraOffset;
+    }
+}
